9035eb764665ff992cccb0a9381924389d61d384
didac89 <126197245+didac89@users.noreply.github.com>
2023-05-22 09:27:28 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,21 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
 
-uint8_t nom=1;
+uint8_t data=1;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial2.begin(115200);
-  Serial2.write(nom);
+  Serial2.write(data);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial2.available() > 0){
-    nom = Serial2.read();
-    Serial.println(nom);
-    Serial2.write(nom); 
+    data = Serial2.read();
+    Serial.println(data);
+    Serial2.write(data); 
   delay(1000);
   }
 }
